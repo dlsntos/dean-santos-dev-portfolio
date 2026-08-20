91727990f9e78d40fec55152d6a4d4fb270686da
Dean <deanlt.santos@gmail.com>
2026-08-20 19:00:36 +0800

    feat(Hero): import hero data and add to text content container

--- src/features/Hero/Hero.jsx
+++ src/features/Hero/Hero.jsx
@@ -1,13 +1,16 @@
-function Hero(){
-    return(
-        <section>
-            <div>
-                
-            </div>
-            <div>
-
-            </div>
-        </section>
-    )
+import { heroData } from "./hero.data.js";
+function Hero() {
+  return (
+    <section>
+      <div></div>
+      <div>
+        <h1>{heroData.name}</h1>
+        <h2>{heroData.title}</h2>
+        <p>{heroData.description.descriptionOne}</p>
+        <p>{heroData.description.descriptionTwo}</p>
+        <p>{heroData.description.descriptionThree}</p>
+      </div>
+    </section>
+  );
 }
 export default Hero;
