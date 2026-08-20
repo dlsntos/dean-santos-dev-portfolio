8db533d8d5d4a894bf1c4b5e1c06c09ac6e4e8a5
Dean <deanlt.santos@gmail.com>
2026-08-20 18:58:39 +0800

    feat(Hero): add new hero.data.js file

---
+++ src/features/Hero/hero.data.js
@@ -0,0 +1,21 @@
+export const heroData = {
+  name: "Dean Louis Santos",
+  title: "Software Developer",
+  description: {
+    descriptionOne: `
+        I am a recent Computer Science Graduate passionate with a passion
+        for developing reliable and efficient software that solves the user's
+        needs. 
+    `,
+    descriptionTwo: `
+        I have a background in development and QA Testing gained through
+        academic and hands-on internship experience,which allows me to view the 
+        development cycle both in the eyes of the developer and tester, ensuring 
+        exceptional software quality.
+    `,
+    descriptionThree: `
+        By bridging these two perspectives, I write 
+        clean code while proactively catching edge cases before they impact the user.
+    `,
+  },
+};
