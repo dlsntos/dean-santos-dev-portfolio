414cbdb911a1d8f67f9b47df1499d0aa19d2aa8c
Dean <deanlt.santos@gmail.com>
2026-08-20 16:40:42 +0800

    feat(header): add new header component

---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Skills</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
