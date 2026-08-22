0b9b93cfd59919fca8fcb2b482a3fee62d77a3bf
Dean <deanlt.santos@gmail.com>
2026-08-23 01:16:54 +0800

    style(About): add flex positioning and padding

--- src/features/About/About.jsx
+++ src/features/About/About.jsx
@@ -1,7 +1,8 @@
 import { aboutData } from "./about.data";
+import styles from "./About.module.css";
 function About() {
   return (
-    <section>
+    <section className={styles.aboutContainer}>
       <h2>About</h2>
       <p>{aboutData.about}</p>
     </section>
